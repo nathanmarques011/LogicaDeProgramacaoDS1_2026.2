dd745aaf5c2c688f532b7b5cfb560b7aa76fd453
nathanmarques011 <nathansmq2010@gmail.com>
2026-09-10 16:15:15 -0300

    feat: codigo de data e nome

--- 01_nocoes_e_fundamentos/olamundo.py
+++ 01_nocoes_e_fundamentos/olamundo.py
@@ -1,2 +1,12 @@
 print("Nathan Marques")
 print("Olá Mundo!")
+
+nome = input("digite seu nome")
+dia = input("digite o dia de seu nascimento")
+mes = input("Digite seu mês de nascimento")
+ano = input("Digite seu ano de nascimento")
+
+print("Meu nome é", nome)
+print("Nasci no dia",dia)
+print("No mês", mes)
+print("No ano de", ano)
